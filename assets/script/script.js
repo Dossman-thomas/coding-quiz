0c8246abb7930910a7c6177e6b166a7d834edb83
Tom Dossman <tdossman.development@gmail.com>
2023-10-18 23:36:37 -0400

    script comments added

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,17 +7,18 @@ SO THAT I can gauge my progress compared to my peers */
 // GLOBAL VARIABLES
 let timeInterval;
 let score; 
+let scoresEl = document.getElementById("scores");
+let timerEl = document.getElementById("timer"); 
+
 // home page variables
 let homeEl = document.getElementById("home"); 
 let startEl = document.getElementById("start"); 
 
 // quiz page variables
 let quizEl = document.getElementById("quiz");
-
-let scoresEl = document.getElementById("scores"); 
-let timerEl = document.getElementById("timer"); 
 let questionEl = document.getElementById("questions");
 let acCheckEl = document.getElementById("answer-check");
+let questionI = 0;
 
 // end page variables 
 let endEl = document.getElementById("finish");
@@ -40,13 +41,24 @@ startEl.addEventListener("click", function(){
   
   homeEl.remove();
   quizEl.style.display = "block";
-  scoresEl.textContent = "View High Scores"
+  // scoresEl.textContent = ""
 
   setTime();
   showQuestions();
   
 });
 
+// automatically displays highscores
+scoresEl.addEventListener("click", function(){
+ homeEl.remove();
+ quizEl.remove();
+ endEl.remove();
+ scoreSectionEl.style.display = "block"; 
+
+ getScores();
+})
+
+// submits user initials and score
 submitEl.addEventListener("click", function(){
 
   endEl.remove();
@@ -56,15 +68,18 @@ submitEl.addEventListener("click", function(){
   getScores();
 })
 
+// retries the quiz
 retryEl.addEventListener("click", function(){
   location.reload();
 })
 
+// clears the leaderboard
 clearEl.addEventListener("click", function(){
   localStorage.setItem("scoreHistory", "[]");
   location.reload();
 })
 
+// saves user score
 function setScore(){
   let initials = inputEl.value;
   let scoreHistory = [];
@@ -79,15 +94,16 @@ function setScore(){
   if (lastStorage !== null){
     scoreHistory = JSON.parse(lastStorage);
   }
-
+  // adds new score to score history
   scoreHistory.push(newScore);
 
   localStorage.setItem("scoreHistory", JSON.stringify(scoreHistory));
 
-  console.log(scoreHistory);
+  // console.log(scoreHistory);
 
 }
 
+// retrieves scores from local storage
 function getScores(){
 
 let scoreHistory = JSON.parse(localStorage.getItem("scoreHistory"));
@@ -106,17 +122,19 @@ var headerRow = tableHeader.insertRow(0);
 headerRow.insertCell(0).innerHTML = '<b>Name</b>';
 headerRow.insertCell(1).innerHTML = '<b>Score</b>';
 
+// inserts values into HTML table retrieved from scoreHistory array
 for (var i = 0; i < scoreHistory.length; i++) {
   var row = table.insertRow(i + 1);
   row.insertCell(0).innerHTML = scoreHistory[i].name;
   row.insertCell(1).innerHTML = scoreHistory[i].score;
 }
 
+// renders table
 leaderboardEl.appendChild(table);
 
 }
  
-// 2) a timer begins counting down. 
+// a timer begins counting down. 
 
 let secondsLeft = 75;
 
@@ -137,7 +155,7 @@ function setTime(){
 }
 
 
-// 3) user presented with a multiple choice question. 
+// user presented with a multiple choice question. 
 
 let questionArray = [
 
@@ -173,8 +191,8 @@ let questionArray = [
 
 ];
 
-let questionI = 0;
 
+// renders questions and multiple choice options on the page one by one
 function showQuestions(){ // inspired by group work w/peers
 
   // if all questions answered or time runs out, game ends and end page is displayed.
@@ -188,7 +206,8 @@ function showQuestions(){ // inspired by group work w/peers
     finalScoreEl.textContent = `Your final score is ${score}.`
 
   } else {
-    
+  
+  // quiz rendering one question at a time
     for (let i = 0; i < questionArray[questionI].options.length; i++){
       
       let optionsEl = document.getElementById(`${i}`);
@@ -200,11 +219,11 @@ function showQuestions(){ // inspired by group work w/peers
         let correctAnswer = questionArray[questionI].correctAnswer;
 
         // if correct, render "correct!" in document and present user with the next question.
-        if ( i === correctAnswer ){
+        if (i === correctAnswer){
           acCheckEl.textContent = "Correct!"
         }
 
-        // if incorrect, render "incorrect!" in document and time subracted from countdown clock. present user with next question.
+        // if incorrect, render "incorrect." in document and time subracted from countdown clock. present user with next question.
         else {
           acCheckEl.innerText = "Incorrect."
           secondsLeft -= 10;
@@ -215,11 +234,3 @@ function showQuestions(){ // inspired by group work w/peers
     }
   }
 }
-
-
-// save user initials and score using local storage.
-
-
-
-
-// display/render user initials and scores from previously saved quiz attempts.
